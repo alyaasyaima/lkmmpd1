18dbf5731b6e3efc6e8d2735f087b7ecedd77061
alyaasyaima <145540019+alyaasyaima@users.noreply.github.com>
2023-09-20 20:53:00 +0700

    Add files via upload

---
+++ LuasLingkaran.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author ACER
+ */
+public class LuasLingkaran {
+    public static void main(String[]args) {
+        final double PI = 3.1415192;
+                double radius = 11.78;
+                double luasLingkaran = PI* radius*radius;
+                
+                System.out.println(luasLingkaran);
+                System.out.println(Math.round(luasLingkaran));
+                System.out.println((int)luasLingkaran);
+    }
+     
+}
